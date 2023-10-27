680524442f964839b56b81fb9b9ef105faf62685
Aswanth Alakkadan <aswanth682@gmail.com>
2023-10-27 15:40:58 +0530

    fire base setup complete

---
+++ src/Firebase/config.js
@@ -0,0 +1,16 @@
+
+import { initializeApp } from 'firebase/app';
+const firebaseConfig = {
+  apiKey: "AIzaSyBlIGBIHCB3LTvgfTjBs4czyTKhdmY0TXI",
+  authDomain: "shaped-manifest-368607.firebaseapp.com",
+  projectId: "shaped-manifest-368607",
+  storageBucket: "shaped-manifest-368607.appspot.com",
+  messagingSenderId: "819866110218",
+  appId: "1:819866110218:web:dda76bba2b3924b0fdd2f3",
+  measurementId: "G-L09EJG18W4"
+};
+
+export default initializeApp(firebaseConfig);
+
+
+
--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { firebaseContext } from './store/firebaseContext';
+import firebase from './Firebase/config'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <firebaseContext.Provider value={{firebase}}>
     <App />
-  </React.StrictMode>
+  </firebaseContext.Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
